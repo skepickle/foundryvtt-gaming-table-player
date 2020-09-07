60835458afd2ef482e348f8f652ef9ef35f128f8
Skepickle <skepickle@users.noreply.github.com>
2020-09-07 22:08:20 +0000

    Add configurable key-binding

--- gaming-table-player.js
+++ gaming-table-player.js
@@ -6,30 +6,55 @@ class PullFocus {
 			name: "Gaming Table's Player Name",
 			hint: "The user name of the player who's session is being displayed on the gaming table",
 			scope: "world",
-			config: true,
 			default: "VTT",
-			type: String
+			type: String,
+			config: true
 		});
-		game.settings.register('gaming-table-player', 'scale', {
-			name: "Scale",
+		game.settings.register('gaming-table-player', 'panscale', {
+			name: "Pan Scale",
 			hint: "The scale at which the map should be locked",
 			scope: "world",
-			config: true,
 			default: 0.5,
-			type: Number
+			type: Number,
+			config: true
 		});
-		game.settings.register('gaming-table-player', 'speed', {
-			name: "Duration (in MS)",
+		game.settings.register('gaming-table-player', 'panspeed', {
+			name: "Pan Duration (in MS)",
 			hint: "How fast or slow to transition to focus point. (1000ms = 1 second)",
 			scope: "world",
-			config: true,
 			default: 250,
-			type: Number
+			type: Number,
 			//onChange: x => window.location.reload()
+			config: true
 		});
+		game.settings.register('gaming-table-player', 'intervalspeed', {
+			name: "Refresh Duration (in MS)",
+			hint: "How fast or slow to refresh gaming table token selections. (1000ms = 1 second)",
+			scope: "world",
+			default: 5000,
+			type: Number,
+			//onChange: x => window.location.reload()
+			config: true
+		});
+		game.settings.register('gaming-table-player', 'keymap', {
+			name: "Keymap",
+			hint: "Enter the keymap used to pull focus on the gaming table",
+			scope: "world",
+			default: "t",
+			type: window.Azzu.SettingsTypes.KeyBinding,
+			config: true
+		});
+		if (game.user.name == game.settings.get('gaming-table-player','player')) {
+			//console.log("setTimeout! " + game.settings.get('gaming-table-player','intervalspeed'));
+			setTimeout(this.gamingTablePlayerLoop, game.settings.get('gaming-table-player','intervalspeed'));
+		}
 	}
 	static async gamingTablePlayerLoop(){
-		//THE FOLLOWING SHOULD MOVE TO A DIFFERENT TRIGGERING MECHANISM
+		if (game.user.name != game.settings.get('gaming-table-player','player')) {
+			//This should never be reached
+			console.log("NOT GAMING TABLE PLAYER: "+ game.settings.get('gaming-table-player','player'));
+			return;
+		}
 		let in_combat = false;
 		let in_combat_id = null;
 		for (let i = 0; i < game.combats.apps.length; i++) {
@@ -46,35 +71,42 @@ class PullFocus {
 		let releaseOth = true;
 		canvas.activeLayer.selectObjects({}, {releaseOthers: true});
 		for (let i = 0; i < canvas.tokens.ownedTokens.length; i++) {
-			if ((!in_combat) ||
+			if (//(!in_combat) ||
 			    (in_combat && (in_combat_id == canvas.tokens.ownedTokens[i].id))) {
 				canvas.tokens.ownedTokens[i].control({releaseOthers: false});
 				releaseOth = false;
 				//console.log("GAME TABLE : "+canvas.tokens.ownedTokens[i].name);
 			}
 		}
-		setInterval(this.gamingTablePlayerLoop, 1000);
+		//console.log("setTimeout!! " + game.settings.get('gaming-table-player','intervalspeed'))
+		setTimeout(function() {
+			PullFocus.gamingTablePlayerLoop();
+		}, game.settings.get('gaming-table-player','intervalspeed'));
 	}
 	static async listen(){
 		game.socket.on('module.gaming-table-player',async data => {
+			this.gamingTablePlayerLoop();
 			if (game.user.name == game.settings.get('gaming-table-player','player')) {
-				await this.gamingTablePlayerLoop();
-				await canvas.animatePan(data)
+				canvas.animatePan(data)
 			}
 		});
 	}
 	static async pullFocus(data){
-		data.scale    = game.settings.get('gaming-table-player','scale');
-		data.duration = game.settings.get('gaming-table-player','speed');
+		data.scale    = game.settings.get('gaming-table-player','panscale');
+		data.duration = game.settings.get('gaming-table-player','panspeed');
 		game.socket.emit('module.gaming-table-player',data)
 	}
 }
 
 var keyDown = (e)=>{
+	const KeyBinding = window.Azzu.SettingsTypes.KeyBinding;
 	//console.log(e.which)
-	console.log('pullfocus keyDown')
+	//console.log('pullfocus keyDown')
 	//TODO Make this use a configurable hotkey istead of just T.
-	if(e.key == "T" && e.altKey && e.ctrlKey && e.shiftKey && game.user.isGM && overCanvas){
+	const parsedValue = KeyBinding.parse(game.settings.get('gaming-table-player','keymap'));
+	const bind = KeyBinding.eventIsForBinding(e, parsedValue);
+	if (bind && game.user.isGM && overCanvas) {
+	//if(e.key == "T" && e.altKey && e.ctrlKey && e.shiftKey && game.user.isGM && overCanvas){
 		//TODO Maybe make sure we're on the right scene before pulling focus.
 		var mouse = canvas.app.renderer.plugins.interaction.mouse.getLocalPosition(canvas.tokens);
 		//console.log(mouse);
